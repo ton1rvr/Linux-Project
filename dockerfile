58b13825e0a03741507116ccb6d4a144e2792836
morganjowitt <jowitt.morgan@gmail.com>
2024-12-03 19:59:01 +0100

    changement sur les chemins des .sh

--- dockerfile
+++ dockerfile
@@ -5,16 +5,13 @@ FROM ubuntu:20.04
 WORKDIR /app
 
 # Copier les scripts dans le conteneur
-COPY setup.sh extraction launch_app.sh streamlit/app.py /app/
+COPY setup.sh extraction/fetch_data.sh launch_app.sh streamlit/app.py /app/
 
 # Rendre les scripts exécutables
 RUN chmod +x /app/setup.sh /app/extraction/fetch_data.sh /app/launch_app.sh
 
 # Exécuter le script setup.sh pour l'installation
 RUN /app/setup.sh
-
-# Créer le répertoire pour les fichiers générés
-RUN mkdir -p /app/data
 
 # Définir le point d'entrée
 ENTRYPOINT ["/bin/bash", "-c", "/app/launch_app.sh"]
--- launch_app.sh
+++ launch_app.sh
@@ -3,11 +3,11 @@
 # Fichier de lancement global pour l'extraction des données et démarrage de l'interface Streamlit
 
 # Donner les permissions d'exécution au script d'extraction
-chmod +x extraction/fetch_data.sh
+chmod +x fetch_data.sh
 
 # Lancer l'extraction des données
 echo "Lancement de l'extraction des données..."
-extraction/fetch_data.sh
+fetch_data.sh
 if [ $? -ne 0 ]; then
   echo "Erreur lors de l'extraction des données."
   exit 1
@@ -16,4 +16,4 @@ echo "Extraction des données terminée avec succès."
 
 # Lancer Streamlit
 echo "Démarrage de l'interface Streamlit..."
-streamlit run streamlit/app.py
+streamlit run app.py
