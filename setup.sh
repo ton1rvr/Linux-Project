cdcde0ed3069e2c588744918a2743eb5845e5ace
morganjowitt <jowitt.morgan@gmail.com>
2024-12-15 16:25:57 +0100

    ajout de matplotlib

--- setup.sh
+++ setup.sh
@@ -32,4 +32,4 @@ esac
 
 # Installer Streamlit
 echo "Installation de Streamlit..."
-pip3 install streamlit pandas plotly arch matplotlib.pyplot
+pip3 install streamlit pandas plotly arch matplotlib
